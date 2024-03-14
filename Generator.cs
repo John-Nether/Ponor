bcd318e9df9fc3bef5803efa0178e6aeebcda7a3
ta2125 <ta2125@LV402>
2024-03-14 10:08:20 +0100

    Přidejte soubory projektu.

---
+++ Generator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponor
+{
+    internal class Generator
+    {
+        public int min { get; set; }
+        public int max { get; set; }
+        public int step { get; set; }
+
+        public Generator(int min, int max, int step)
+        {
+
+            this.min = min;
+            this.max = max;
+            this.step = step;
+
+        }
+
+        public double[,] GenDat()
+        {
+            Random ran = new Random();
+            double[,] tmp = new double[2, step];
+            for (int i = 0; i < 2; i++)
+            {
+
+                for (int j = 0; j < step; j++)
+                {
+                    tmp[i, j] = ran.NextDouble();
+                }
+            }
+            return tmp;
+        }
+    }
+}
---
+++ Log.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponor
+{
+    internal struct Log
+    {
+        public double[] log { get; set; }
+        public Log(double[] log )
+        {
+            
+            this.log = log;
+        }
+
+        public void newLog(List<int> t, List<double> h) { 
+        
+        
+        
+        }
+    }
+}
